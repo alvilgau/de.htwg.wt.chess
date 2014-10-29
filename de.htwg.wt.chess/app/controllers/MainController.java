cebfa016f7eb62ee39638fe5597d283754b3b9d0
Alexander Vilgauk <vilgauk.alex@gmail.com>
2014-10-29 16:32:35 +0100

    added new menus

--- de.htwg.wt.chess/app/controllers/MainController.java
+++ de.htwg.wt.chess/app/controllers/MainController.java
@@ -10,12 +10,20 @@ public class MainController extends Controller {
 	static IChessController controller = Chess.getInstance().getController();
 
 	public static Result index() {
-		return ok(views.html.index.render(controller));
+		return ok(views.html.index.render());
+	}
+
+	public static Result chess() {
+		return ok(views.html.chess.render(controller));
+	}
+
+	public static Result contact() {
+		return ok(views.html.contact.render());
 	}
 
 	public static Result commandline(String command) {
 		Chess.getInstance().getTui().processInputLine(command);
-		return ok(views.html.index.render(controller));
+		return ok(views.html.chess.render(controller));
 	}
 
 }

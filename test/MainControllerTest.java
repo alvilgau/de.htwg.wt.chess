889bee9b736a8f7cecde35f3440894d266cd089c
Alexander Vilgauk <vilgauk.alex@gmail.com>
2014-12-22 18:48:13 +0100

    modified controller test

--- test/MainControllerTest.java
+++ test/MainControllerTest.java
@@ -1,25 +1,21 @@
-import static org.fest.assertions.Assertions.assertThat;
-import static play.mvc.Http.Status.OK;
-import static play.test.Helpers.callAction;
-import static play.test.Helpers.charset;
-import static play.test.Helpers.contentAsString;
-import static play.test.Helpers.contentType;
-import static play.test.Helpers.fakeApplication;
-import static play.test.Helpers.running;
-import static play.test.Helpers.status;
+import org.junit.*;
 
-import org.junit.Test;
-
-import play.mvc.Result;
+import play.mvc.*;
+import static play.test.Helpers.*;
+import static org.fest.assertions.Assertions.*;
 import controllers.routes;
 
-/**
- *
- * Simple (JUnit) tests that can call all parts of a Play app. If you are
- * interested in mocking a whole application, see the wiki for more details.
- *
- */
 public class MainControllerTest {
+
+	@Before
+	public void setUp() {
+		start(fakeApplication(inMemoryDatabase(), fakeGlobal()));
+	}
+
+	@After
+	public void tearDown() {
+		stop(fakeApplication());
+	}
 
 	@Test
 	public void simpleCheck() {
@@ -29,16 +25,11 @@ public class MainControllerTest {
 
 	@Test
 	public void indexShouldContainTheCorrectString() {
-		running(fakeApplication(), new Runnable() {
-			public void run() {
-				Result result = callAction(routes.ref.MainController.index());
-				assertThat(status(result)).isEqualTo(OK);
-				assertThat(contentType(result)).isEqualTo("text/html");
-				assertThat(charset(result)).isEqualTo("utf-8");
-				assertThat(contentAsString(result))
-						.contains("Welcome to Chess");
-			}
-		});
+		Result result = callAction(routes.ref.MainController.index());
+		assertThat(status(result)).isEqualTo(OK);
+		assertThat(contentType(result)).isEqualTo("text/html");
+		assertThat(charset(result)).isEqualTo("utf-8");
+		assertThat(contentAsString(result)).contains("Welcome to Chess");
 	}
 
 }

54bcb653fd1aa88ffabf5aa199778d92c4f0746c
Alexander Vilgauk <vilgauk.alex@gmail.com>
2015-02-18 19:41:15 +0100

    lobby notification

--- app/controllers/MainController.java
+++ app/controllers/MainController.java
@@ -154,6 +154,7 @@ public class MainController extends JavaController {
 										.toString());
 								notifyPlayersInLobby();
 							}
+							playersInLobby.remove(player);
 							player.clear();
 						}
 					}
